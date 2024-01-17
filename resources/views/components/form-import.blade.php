af763988244f4f41989d48eb320b36d6c1ef277a
fatihjr9 <malikfatihibrahim@gmail.com>
2024-01-17 14:55:04 +0700

    Merge and export left

--- resources/views/components/form-import.blade.php
+++ resources/views/components/form-import.blade.php
@@ -1,8 +1,8 @@
 <form action="{{ route('dashboard.preview') }}" method="post" enctype="multipart/form-data" class="flex flex-col space-y-4">
     @csrf
     <div class="grid grid-cols-2 gap-4">
-        <input name="data_pertama" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input_1" type="file">
-        <input name="data_kedua" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input_2" type="file">
+        <input name="data_pertama" class="block w-full px-4 py-2 text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input_1" type="file">
+        <input name="data_kedua" class="block w-full px-4 py-2 text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input_2" type="file">
     </div>
     <div class="flex flex-row gap-2">
         <button class="py-2 rounded-md w-full bg-indigo-600" type="submit">Submit</button>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,20 +7,12 @@
 
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="grid grid-cols-1 md:grid-cols-2 dark:text-white gap-6">
-                <div class="bg-gray-800 p-4 rounded-md">
-                    <div class="flex flex-col space-y-1 border-b pb-2 border-gray-700 mb-2">
-                        <h5 class="text-xl font-bold">Import File Excel</h5>
-                        <p class="text-sm text-gray-500">Format yang didukung : CSV, XSLX</p>
-                    </div>
-                    <x-form-import data="[$data1, $data2]"/>
+            <div class="bg-gray-800 p-4 rounded-md">
+                <div class="flex flex-col space-y-1 border-b pb-2 border-gray-700 mb-2">
+                    <h5 class="text-2xl font-bold text-white">Import File Excel</h5>
+                    <p class="text-sm text-slate-400">Format yang didukung : CSV, XSLX</p>
                 </div>
-                <div class="bg-gray-800 p-4 rounded-md">
-                    <div class="flex flex-col space-y-1">
-                        <h5 class="text-xl font-bold">Hasil File Excel</h5>
-                        <p class="text-sm text-gray-500">Format yang didukung : CSV, XSLX</p>
-                    </div>
-                </div>
+                <x-form-import data="[$data1, $data2]"/>
             </div>
         </div>
     </div>
